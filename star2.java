851070b21e7b1ddc920bd58379c4accfb17fd630
Harsh Sharma <117378407+GitHub-Harsh-Sharma@users.noreply.github.com>
2022-12-17 14:50:50 +0530

    Add files via upload

---
+++ star2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class Star2
+{
+ public static void main(String[] args)
+  {
+    int i,k,s,a=1,b=2;
+    for(i=1; i<=3;i++)
+    {
+        for(k=1;k<=a;k++)
+        {
+            System.out.print("*");
+        }
+        System.out.println("");
+        a=a+2;
+    }    
+    }
+}
